282e0c9f867c2c900fab00bd67c234efc8b6d29a
Yusuke Izumi <yuizumi@y5i.org>
2016-08-06 14:13:31 +0900

    Added /r:System.Numerics.

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+declare -ar REFS=(
+    /r:System.Numerics
+)
 declare -ar DEPS=(
     /r:packages/Newtonsoft.Json.9.0.1/lib/net45/Newtonsoft.Json.dll
     /r:packages/Rationals.1.2.0/lib/net461/Rationals.dll
@@ -11,7 +14,7 @@ compile() {
     local name="$1" ; shift
     ln -f "${DEPS[@]/\/r:/}" bin
     mono --debug nflat/bin/nf.exe "${DEPS[@]/\/r:/}" "$@" > "bin/${name}.cs"
-    mcs /debug+ "${DEPS[@]}" "bin/${name}.cs"
+    mcs /debug+ "${REFS[@]}" "${DEPS[@]}" "bin/${name}.cs"
 }
 
 cd "${0%/*}"
